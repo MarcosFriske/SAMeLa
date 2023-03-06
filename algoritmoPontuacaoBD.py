e555b279b9da5d104a68c428744e7b3bd0e41d3a
Marcos Felipe Friske dos Santos <64426342+MarcosFriske@users.noreply.github.com>
2023-03-05 21:47:58 -0300

    Criado script inicial que usa o banco de dados para avaliacao
    Terminei o primeiro protótipo do script que usa o banco de dados para realizar avaliação.

---
+++ algoritmoPontuacaoBD.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 18:27:46 2023
+
+@author: Marcos
+"""
+
+############### IMPORTS ############### 
+
+from sys import exit
+import psycopg2
+from lxml import etree
+import pandas as pd
+
+############### CONFIGS BD ############### 
+
+## Credencias conexão ao banco de dados
+
+host_db = "localhost"
+port_db = "5432"
+database_name = "valida_lattes"
+user_db = "postgres"
+password_db = "admin"
+
+# Configurando o filtro de ano inicial e final para ser utilizado nos XPATHS do BD
+min_ano = 1900
+max_ano = 2023
+
+############### FUNÇÕES ############### 
+
+# Definição da função para selecionar os criterios de avaliacao do instrumento de avaliacao informado
+def selecionar_criterios_avaliacao(instrumento_avaliacao):
+    try:
+        connection = psycopg2.connect(user=user_db,
+                                      password=password_db,
+                                      host=host_db,
+                                      port=port_db,
+                                      database=database_name)
+        cursor = connection.cursor()
+        postgres_select_query = """SELECT * FROM public.criterios
+                                   WHERE ativo = true AND fk_id_instrumento_avaliacao = %s
+                                   ORDER BY id_criterio"""
+        instrumento_avaliacao_select = (instrumento_avaliacao)
+        cursor.execute(postgres_select_query, instrumento_avaliacao_select)
+        rows = cursor.fetchall()
+        if rows:
+            df = pd.DataFrame(rows, columns=[desc[0] for desc in cursor.description])
+            print(f"{len(df.index)} records selected successfully from Criterios table")
+            return df
+        else:
+            print("No rows found")
+            return None
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to select records from Criterios table", error)
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+            
+# Definição da função para carregar o conteúdo da coluna lattes_xml da tabela servidores
+def carregar_lattes_xml(id_servidor):
+    try:
+        connection = psycopg2.connect(user=user_db,
+                                      password=password_db,
+                                      host=host_db,
+                                      port=port_db,
+                                      database=database_name)
+        cursor = connection.cursor()
+        postgres_select_query = """SELECT lattes_xml FROM public.servidores
+                                WHERE id_servidor = %s"""
+        id_servidor_select = (id_servidor)
+        cursor.execute(postgres_select_query, id_servidor_select)
+        rows = cursor.fetchall()
+        if rows:
+            xml_element = etree.fromstring(rows[0][0])
+            print("Record selected successfully from Servidores table")
+            return xml_element
+        else:
+            print("No rows found")
+            return None
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to select records from Servidores table", error)
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
+            
+# Definição da função para carregar os títulos similares da tabela Qualis
+def selecionar_titulos_similares(titulo, min_similaridade):
+    try:
+        connection = psycopg2.connect(user=user_db,
+                                      password=password_db,
+                                      host=host_db,
+                                      port=port_db,
+                                      database=database_name)
+        cursor = connection.cursor()
+        postgres_select_query = """SELECT titulo FROM public.qualis
+                                   WHERE similarity(titulo, %s) > %s"""
+        cursor.execute(postgres_select_query, (titulo, min_similaridade))
+        rows = cursor.fetchall()
+        if rows:
+            titulos_qualis = [row[0] for row in rows]
+            return titulos_qualis
+        else:
+            return []
+    except (Exception, psycopg2.Error) as error:
+        print("Failed to select similar titles from tabela qualis", error)
+        return []
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+############### EXECUÇÃO ############### 
+
+# Começar a execução perguntando por um servidor, verificando se o mesmo tem um XML salvo nos seus dados na tabela servidores.
+    
+id_servidor = input("Digite o ID do servidor que deseja consultar: ")
+xml = carregar_lattes_xml(id_servidor)
+
+if xml is None:
+    print(f"O servidor com o ID: {id_servidor} não possui nenhum XML de currículo Lattes associado na coluna lattes_xml.")
+    exit(1)
+
+# Continuar a execução do programa utilizando o XML retornado
+            
+# Solicitação do número do instrumento de avaliação ao usuário
+instrumento_avaliacao_id = input("Informe o número do ID do instrumento de avaliação: ")
+
+# Execução da função com o ID informado pelo usuário
+df_criterios_avaliacao = selecionar_criterios_avaliacao(instrumento_avaliacao_id)
+
+# Verificação do dataframe retornado pela função
+if df_criterios_avaliacao is not None:
+    print(df_criterios_avaliacao['criterio'])
+    
+    df_avaliacao = pd.DataFrame(columns=['item', 'criterios', 'pontuacao_por_item', 'pontuacao_maxima', 'quantidade', 'pontuacao_atingida'])
+    
+    for index, row in df_criterios_avaliacao.iterrows():
+        xpath_criterio_lattes = row['xpath_criterio_lattes']
+        
+        if row['considera_qualis'] == False:
+            if 'min_ano' in xpath_criterio_lattes and 'max_ano' in xpath_criterio_lattes:
+                xpath = etree.XPath(xpath_criterio_lattes)
+                count = xpath(xml, min_ano=min_ano, max_ano=max_ano)
+                print(xpath)
+                print(count)
+                
+                # obter a próxima linha vazia do dataframe
+                next_row = len(df_avaliacao)
+                # adicionar os valores à próxima linha vazia
+                df_avaliacao.loc[next_row] = [index, row['criterio'], row['pontuacao_item'], (row['pontuacao_item']*row['qtd_maxima_itens']), count, (row['pontuacao_item']*count)]
+                
+            elif 'min_ano' not in xpath_criterio_lattes and 'max_ano' not in xpath_criterio_lattes:
+                xpath = etree.XPath(xpath_criterio_lattes)
+                count = xpath(xml)
+                print(xpath)
+                print(count)
+                
+                # obter a próxima linha vazia do dataframe
+                next_row = len(df_avaliacao)
+                # adicionar os valores à próxima linha vazia
+                df_avaliacao.loc[next_row] = [index, row['criterio'], row['pontuacao_item'], (row['pontuacao_item']*row['qtd_maxima_itens']), count, (row['pontuacao_item']*count)]
+            
+        elif row['considera_qualis']:
+            if xpath_criterio_lattes == 'sem_qualis':
+                # Por enquanto o tratamento com e sem_qualis é feito tudo de uma vez só. Talvez depois separar.
+                continue
+            else:
+                expressao_xpath = xpath_criterio_lattes
+                resultados_xpath = xml.xpath(expressao_xpath, min_ano=min_ano, max_ano=max_ano)
+                
+                # Extrair os dados de cada resultado XPath e adicionar em uma lista
+                dados = []
+                for resultado in resultados_xpath:
+                    dados_artigo = {}
+                    dados_basicos_artigo = resultado.find("DADOS-BASICOS-DO-ARTIGO")
+                    dados_artigo['natureza'] = dados_basicos_artigo.get('NATUREZA')
+                    dados_artigo['titulo'] = dados_basicos_artigo.get('TITULO-DO-ARTIGO')
+                    dados_artigo['ano'] = dados_basicos_artigo.get('ANO-DO-ARTIGO')
+                    dados_artigo['pais_publicacao'] = dados_basicos_artigo.get('PAIS-DE-PUBLICACAO')
+                    dados_artigo['idioma'] = dados_basicos_artigo.get('IDIOMA')
+                    dados_artigo['meio_divulgacao'] = dados_basicos_artigo.get('MEIO-DE-DIVULGACAO')
+                    dados_artigo['home_page'] = dados_basicos_artigo.get('HOME-PAGE-DO-TRABALHO')
+                    dados_artigo['flag_relevancia'] = dados_basicos_artigo.get('FLAG-RELEVANCIA')
+                    dados_artigo['doi'] = dados_basicos_artigo.get('DOI')
+                    dados_artigo['titulo_ingles'] = dados_basicos_artigo.get('TITULO-DO-ARTIGO-INGLES')
+                    dados_artigo['flag_divulgacao_cientifica'] = dados_basicos_artigo.get('FLAG-DIVULGACAO-CIENTIFICA')
+                    dados.append(dados_artigo)
+
+                # Criar um DataFrame do pandas com os dados extraídos
+                df_artigos_xml = pd.DataFrame(dados)
+
+                # Mostrar o DataFrame
+                print(df_artigos_xml)
+                
+                # Criar uma lista vazia para armazenar os resultados da verificação
+                existe_titulo_similar = []
+                
+                # Para cada linha do dataframe 'df_artigos_xml', verificar se o título está presente na tabela 'qualis'
+                for idx, row_artigo in df_artigos_xml.iterrows():
+                    titulo = row_artigo['titulo']
+                    titulos_qualis = selecionar_titulos_similares(titulo, 0.7)
+                    if titulos_qualis:
+                        existe_titulo_similar.append(True)
+                    else:
+                        existe_titulo_similar.append(False)
+                
+                # Adicionar a lista de resultados como uma nova coluna no dataframe 'df_artigos_xml'
+                df_artigos_xml['existe_titulo_similar'] = existe_titulo_similar
+                
+                # Contagem de artigos com e sem qualis
+                contagem_artigos_com_qualis = df_artigos_xml['existe_titulo_similar'].value_counts().get(True, 0)
+                contagem_artigos_sem_qualis = df_artigos_xml['existe_titulo_similar'].value_counts().get(False, 0)
+                
+                # Imprimir os resultados
+                print("Quantidade de artigos com similaridade no título em relação à tabela_qualis: ", contagem_artigos_com_qualis)
+                print("Quantidade de artigos sem similaridade no título em relação à tabela_qualis: ", contagem_artigos_sem_qualis)
+                
+                ##### Por enquanto tá tratando com e sem qualis aqui, ver como separar depois.
+                # Preencher o dataframe df_avaliacao com os dados disponíveis referente ao Qualis
+                # obter a próxima linha vazia do dataframe
+                next_row = len(df_avaliacao)
+                # adicionar os valores à próxima linha vazia
+                df_avaliacao.loc[next_row] = [index, row['criterio'], row['pontuacao_item'], (row['pontuacao_item']*row['qtd_maxima_itens']), count, (row['pontuacao_item']*count)]
+                # obter a próxima linha vazia do dataframe
+                next_row = len(df_avaliacao)
+                # adicionar os valores à próxima linha vazia
+                df_avaliacao.loc[next_row] = [index, 'Artigo completo publicado em periódico especializado, sem Qualis', row['pontuacao_item'], (row['pontuacao_item']*row['qtd_maxima_itens']), contagem_artigos_sem_qualis, (row['pontuacao_item']*count)]
+
+# após preencher o dataframe df_avaliacao
+
+nome_arquivo_excel = 'avaliacao.xlsx'
+
+with pd.ExcelWriter(nome_arquivo_excel) as writer:
+    df_avaliacao.to_excel(writer, sheet_name='Avaliação', index=False)
+    
+print(f'Dataframe salvo em {nome_arquivo_excel}')
+
+if df_criterios_avaliacao is None:
+    print(f"Não foi possível encontrar o instrumento de avaliação associado ao ID: {instrumento_avaliacao_id} informado.")
+    exit(1)
+    
+    
